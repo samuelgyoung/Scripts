ed6d9be2c899318b08b9ad1f106b174d696982a7
samuelgyoung <samuelgyoung@gmail.com>
2017-06-01 11:49:42 -0500

    Added chmod to execute scripts

--- cloneScriptsRepo.sh
+++ cloneScriptsRepo.sh
@@ -6,9 +6,11 @@ if [ -d ~/bin/ ]; then
 	cd ~/bin/ && rm -fr *; rm -fr .*
 	cd ~ && 
 	git clone https://github.com/samuelgyoung/Scripts bin
+	cd ~/bin/ && chmod 755 *
 else
 	echo "Directory does NOT exist. Cloning the repo."
 	git clone https://github.com/samuelgyoung/Scripts bin
+	cd ~/bin/ && chmod 755 *
 fi
 
 

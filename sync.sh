0a5107e69483404a2524b708abc4c8a34a1c566c
sgyoung <sgyoung@192.168.5.198>
2020-01-02 16:20:05 -0600

    Added more mouunts

--- sync.sh
+++ sync.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
 rsync -av /Volumes/sgyoung/tmp /Volumes/extendDrive/sgyoung/tmp
+rsync -av /Volumes/sgyoung/Videos /Volumes/extendDrive/sgyoung/Videos
+rsync -av /Volumes/sgyoung/MUTHER6 /Volumes/extendDrive/sgyoung/MUTHER6
+rsync -av /Volumes/sgyoung/Documents /Volumes/extendDrive/sgyoung/Documents
+rsync -av /Volumes/sgyoung/Books /Volumes/extendDrive/sgyoung/Books
+rsync -av /Volumes/sgyoung/Backup /Volumes/extendDrive/sgyoung/Backup
 rsync -av /Volumes/Binary/ /Volumes/extendDrive/Binary/
+rsync -av /Volumes/Video/ /Volumes/extendDrive/Video
